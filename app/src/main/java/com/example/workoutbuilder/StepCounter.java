bb3acd25a7eba77f8b372fbe554cb36337b4addf
xSmoked <xsmoked@xSmokeds-MacBook-Pro.local>
2023-11-16 14:27:20 +0000

    Added the phone asking for permission to use step counter sensor

--- app/src/main/java/com/example/workoutbuilder/StepCounter.java
+++ app/src/main/java/com/example/workoutbuilder/StepCounter.java
@@ -1,17 +1,25 @@
 package com.example.workoutbuilder;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.Manifest;
+import android.widget.Toast;
 
 public class StepCounter extends AppCompatActivity implements SensorEventListener{
 
@@ -20,8 +28,11 @@ public class StepCounter extends AppCompatActivity implements SensorEventListene
     private SensorManager sensorManager;
     private Sensor stepCounterSensor;
 
+    private static final int MY_PERMISSIONS_REQUEST_ACTIVITY_RECOGNITION = 123;
+
     private int stepCount = 0;
 
+    @RequiresApi(api = Build.VERSION_CODES.Q)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +42,18 @@ public class StepCounter extends AppCompatActivity implements SensorEventListene
         workoutsButton = findViewById(R.id.btnWorkouts);
         stepCounterTV = findViewById(R.id.stepCountTV);
 
+
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACTIVITY_RECOGNITION
+        ) != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted, request it
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[]{Manifest.permission.ACTIVITY_RECOGNITION},
+                    MY_PERMISSIONS_REQUEST_ACTIVITY_RECOGNITION
+            );
+        }
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         stepCounterSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
@@ -87,4 +110,25 @@ public class StepCounter extends AppCompatActivity implements SensorEventListene
     public void onAccuracyChanged(Sensor sensor, int i ){
 
     }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode,
+            @NonNull String[] permissions,
+            @NonNull int[] grantResults
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == MY_PERMISSIONS_REQUEST_ACTIVITY_RECOGNITION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted, register the sensor listener
+                if (stepCounterSensor != null) {
+                    sensorManager.registerListener(this, stepCounterSensor, SensorManager.SENSOR_DELAY_FASTEST
+                    );
+                }
+            } else {
+                Toast.makeText(StepCounter.this,"You need physical activity permissions to use this feature", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
 }
